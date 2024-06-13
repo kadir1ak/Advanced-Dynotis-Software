0b91cef091242e2102a22bf50fc4db229df11cc2
Kadir AK <kadir.akx@gmail.com>
2024-06-13 10:49:16 +0300

    t2

--- ViewModels/Pages/SingleTestViewModel.cs
+++ ViewModels/Pages/SingleTestViewModel.cs
@@ -1,12 +1,72 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Advanced_Dynotis_Software.Models.Serial;
+using Advanced_Dynotis_Software.ViewModels.Device;
+using Advanced_Dynotis_Software.Services.Helpers;
 
 namespace Advanced_Dynotis_Software.ViewModels.Pages
 {
-    class SingleTestViewModel
+    public class SingleTestViewModel : INotifyPropertyChanged
     {
+        private SerialPortsManager _serialPortsManager;
+        private string _selectedPort;
+        private ObservableCollection<DeviceViewModel> _connectedDevices;
+
+        public ObservableCollection<string> AvailablePorts { get; set; }
+        public ObservableCollection<DeviceViewModel> ConnectedDevices
+        {
+            get => _connectedDevices;
+            set
+            {
+                _connectedDevices = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string SelectedPort
+        {
+            get => _selectedPort;
+            set
+            {
+                _selectedPort = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand ConnectCommand { get; set; }
+
+        public SingleTestViewModel()
+        {
+            _serialPortsManager = new SerialPortsManager();
+            AvailablePorts = new ObservableCollection<string>(_serialPortsManager.GetSerialPorts());
+            _connectedDevices = new ObservableCollection<DeviceViewModel>();
+            _serialPortsManager.SerialPortsEvent += OnSerialPortsChanged;
+            ConnectCommand = new RelayCommand(ConnectToDevice);
+        }
+
+        private void OnSerialPortsChanged()
+        {
+            AvailablePorts = new ObservableCollection<string>(_serialPortsManager.GetSerialPorts());
+        }
+
+        private void ConnectToDevice(object parameter)
+        {
+            if (!string.IsNullOrEmpty(SelectedPort))
+            {
+                var deviceViewModel = new DeviceViewModel(SelectedPort);
+                _connectedDevices.Clear();
+                _connectedDevices.Add(deviceViewModel);
+                
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
