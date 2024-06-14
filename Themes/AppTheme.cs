0c8cc41dff94dc065b07e508f81b13a585dd4c17
Kadir AK <kadir.akx@gmail.com>
2024-06-14 18:11:45 +0300

    t6.3

--- Themes/AppTheme.cs
+++ Themes/AppTheme.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
 
 namespace Advanced_Dynotis_Software.Themes
 {
@@ -9,9 +11,31 @@ namespace Advanced_Dynotis_Software.Themes
 
         public static void ChangeTheme(Uri themeUri)
         {
-            ResourceDictionary theme = new ResourceDictionary() { Source = themeUri };
-            Application.Current.Resources.Clear();
-            Application.Current.Resources.MergedDictionaries.Add(theme);
+            ResourceDictionary newTheme = new ResourceDictionary() { Source = themeUri };
+            ResourceDictionary oldTheme = Application.Current.Resources.MergedDictionaries[0];
+
+            foreach (var key in newTheme.Keys)
+            {
+                if (newTheme[key] is SolidColorBrush newBrush && oldTheme[key] is SolidColorBrush oldBrush)
+                {
+                    // Create a copy of the current brush to avoid the frozen brush issue
+                    SolidColorBrush animatedBrush = new SolidColorBrush(oldBrush.Color);
+                    animatedBrush.BeginAnimation(SolidColorBrush.ColorProperty, new ColorAnimation
+                    {
+                        From = oldBrush.Color,
+                        To = newBrush.Color,
+                        Duration = new Duration(TimeSpan.FromMilliseconds(250))
+                    });
+
+                    // Replace the frozen brush with the new animated brush
+                    Application.Current.Resources[key] = animatedBrush;
+                }
+            }
+
+            // Clear the old theme and add the new theme
+            Application.Current.Resources.MergedDictionaries.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(newTheme);
+
             ThemeChanged?.Invoke(null, EventArgs.Empty);
         }
     }
