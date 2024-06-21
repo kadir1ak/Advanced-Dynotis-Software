f372794977a82a511bf54b481e1a590fed609d96
Kadir AK <kadir.akx@gmail.com>
2024-06-21 16:09:24 +0300

    t9.9.1

---
+++ Services/Helpers/ValueToWidthConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Advanced_Dynotis_Software.Services.Helpers
+{
+    public class ValueToWidthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double val = (double)value;
+            return val * 3; // Adjust this multiplier as needed for your application.
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
