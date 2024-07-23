7adb93af3df27c770ee085b6cdaeff49384c9f08
Kadir AK <kadir.akx@gmail.com>
2024-07-23 15:59:36 +0300

    v.1.2.4 Balancer kullanıcı hataları önlendi

--- Views/UserControls/BalancerParameters.xaml.cs
+++ Views/UserControls/BalancerParameters.xaml.cs
@@ -1,18 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Advanced_Dynotis_Software.Views.UserControls
 {
@@ -25,6 +14,7 @@ namespace Advanced_Dynotis_Software.Views.UserControls
         {
             InitializeComponent();
         }
+
         private void NumericTextBoxDouble_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
             Regex regex = new Regex("[^0-9.,]+"); // Allow only numbers, dots, and commas
@@ -35,6 +25,24 @@ namespace Advanced_Dynotis_Software.Views.UserControls
         {
             Regex regex = new Regex("[^0-9]+"); // Allow only numbers
             e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void NumericTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox.Text.Length > 9)
+            {
+                textBox.Text = textBox.Text.Substring(0, 9);
+                textBox.CaretIndex = textBox.Text.Length; // Move caret to end
+            }
+        }
+
+        private void NumericTextBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+            {
+                e.Handled = true; // Prevent space key input
+            }
         }
     }
 }
